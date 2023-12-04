a486d156b7c5e71938b3063a674394d6f5923c52
BFORD1516 <151584741+BFORD1516@users.noreply.github.com>
2023-12-04 15:49:22 -0800

    Add files via upload

---
+++ LetandConst.js
@@ -0,0 +1,16 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!=
+
+const PI = 3.14;
+let variableName = 3.14;
+variableName = 42;
+
+//What is the difference between var and let?
+//•	In JavaScript, both var and let are used for variable declaration, but there are some key differences between them. These differences primarily relate to their scoping behavior.
+//What is the difference between var and const?
+//•	Variables declared with var can be reassigned and updated throughout their scope.
+//•	Variables declared with const cannot be reassigned once they have been assigned a value.
+//What is the difference between var and const?
+//•	In modern JavaScript, it is recommended to use const by default and only use let when you need to reassign a variable.
+//What is hoisting?
+//•	Hoisting is a behavior in JavaScript where variable and function declarations are moved to the top of their containing scope during the compilation phase. This means that you can use a variable or call a function before it's declared in the code.
